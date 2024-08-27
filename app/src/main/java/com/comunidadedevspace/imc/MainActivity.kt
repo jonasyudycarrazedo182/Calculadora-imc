ab0598d8af4aecc5cddabf004a59c06d49d28223
Jonas <jonasyudycarrazedo182@gmail.com>
2024-08-27 14:54:42 -0300

    mostrar mensagem preencha os campos

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -3,6 +3,7 @@ package com.comunidadedevspace.imc
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
@@ -17,27 +18,44 @@ class MainActivity : AppCompatActivity() {
 // Recuperar o texto digitado no edt peso
 
 
-    val edtpeso = findViewById<TextInputEditText>(R.id.edt_peso)
-    val edtAltura = findViewById<TextInputEditText>(R.id.edt_altura)
+        val edtPeso = findViewById<TextInputEditText>(R.id.edt_peso)
+        val edtAltura = findViewById<TextInputEditText>(R.id.edt_altura)
 
-    val btnCalcular = findViewById<Button>(R.id.btn_calcular)
-
-
-    btnCalcular.setOnClickListener {
-        val peso: Float = edtpeso.text.toString().toFloat()
-        val altura = edtAltura.text.toString().toFloat()
-
-        val alturaQ2 = altura * altura
-        val resultado = peso /alturaQ2
-
-        println("Roque acao do botao " + resultado)
+        val btnCalcular = findViewById<Button>(R.id.btn_calcular)
 
 
+        btnCalcular.setOnClickListener {
 
-    }
 
+            val pesoStr: String = edtPeso.text.toString()
+            val alturaStr: String = edtAltura.text.toString()
+
+            if (pesoStr == "" || alturaStr == "")  {
+                // Mostrar mensagem para o usuario
+                Snackbar.make(
+                        edtPeso,
+                        "Preencha todos os campos",
+                        Snackbar.LENGTH_LONG
+                    )
+                    .show()
+
+            } else {
+                val peso = pesoStr.toFloat()
+                val altura = alturaStr.toFloat()
+
+                val alturaQ2 = altura * altura
+                val resultado = peso / alturaQ2
+
+                println("Roque acao do botao " + resultado)
+            }
+        }
     }
 }
+
+
+
+
+
 
 
 
